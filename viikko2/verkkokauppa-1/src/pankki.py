29a428428d9b5aec291a61dc5721ab0cb92939f1
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2022-11-11 14:13:07 +0200

    viikko2

---
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -0,0 +1,15 @@
+from kirjanpito import kirjanpito as default_kirj
+
+class Pankki:
+    def __init__(self, kirjanpito=default_kirj):
+        self._kirjanpito = kirjanpito
+
+    def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
+        self._kirjanpito.lisaa_tapahtuma(
+            f"tilisiirto: tililtä {tililta} tilille {tilille} viite {viitenumero} summa {summa}e"
+        )
+
+        # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return True
+
+pankki = Pankki()

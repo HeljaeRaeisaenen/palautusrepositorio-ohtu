be6a2e322502fe8dcfd4eee60f45b192eca8b509
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2022-11-23 19:34:35 +0200

    lisätty testi 5 mutta kaikki toimii jo

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -24,8 +24,16 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
-    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korin_hinta_oikein(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+    
+    def test_2_tuotteen_lisaamisen_jalkeen_korin_hinta_oikein(self):
+        maito = Tuote("Maito", 3)
+        leipä = Tuote('Jälkiuuni Viipaleet', 5)
+        self.kori.lisaa_tuote(leipä)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 8)
